636e50d4550e0aeda5e383bbf70347af3a595890
aadhar-jain <aadhar.jain@voylla.com>
2018-09-04 22:55:51 +0530

    Modularised code and completed all the conditions

--- js/index.js
+++ js/index.js
@@ -24,8 +24,8 @@ $(document).ready(function(){
 
 	function load(){
 		setTimeout(function(){
-			$('#splash-screen').addClass('hidden');
-		}, 3000);
+			$('#splash-screen').fadeOut(1000);
+		}, 2000);
 		$('#input-submit-btn').click(function(e){
 			var answer = $('#user-input').val();
 			if(answer){
@@ -35,149 +35,136 @@ $(document).ready(function(){
 		});
 		questionNo = 1;
 		var chatOutputSection = $('#chat-output-section'), response = '';
-		response = `
-			<div class="message flex-row flex-start">
-				<div class="bot-msg flex-row flex-center">
-					Hi how are you
-				</div>
-			</div>
-		`;
+		response = getBotMsgTemplate('Hi how are you');
 		sendResponse(chatOutputSection, response);
 	}
 
 	function postAnswer(answer){
 		var chatOutputSection = $('#chat-output-section');
-		var post = `
-			<div class="message flex-row flex-end">
-				<div class="user-msg flex-row flex-center">
-					` + answer + `
-				</div>
-			</div>
-		`;
+		var post = getUserMsgTemplate(answer);
 		chatOutputSection.append(post);
+		chatOutputSection.scrollTop(chatOutputSection.height());
 		$('#user-input').val('');
 	}
 
 	function sendResponse(chatOutputSection, response){
 		chatOutputSection.append(response);
+		chatOutputSection.scrollTop(chatOutputSection.height());
+	}
+
+	function getUserMsgTemplate(msg){
+		var template = `
+			<div class="message flex-row flex-end">
+				<div class="user-msg flex-row flex-center">
+					` + msg + `
+				</div>
+			</div>
+		`;
+		return template;
+	}
+
+	function getBotMsgTemplate(msg){
+		var template = `
+			<div class="message flex-row flex-start">
+				<div class="bot-msg flex-row flex-center">
+					` + msg + `
+				</div>
+			</div>
+		`;
+		return template;
+	}
+
+	function getCarousalTemplate(){
+		var template = `
+			<div class="flex-row" id="carousal-ctn">
+				<div class="carousal-img-ctn"><img src="./img/dandruff.jpg" class="carousal-img" alt="Dove Shampoo" /></div>
+				<div class="carousal-img-ctn"><img src="./img/hairfall.jpg" class="carousal-img" alt="Dove Shampoo" /></div>
+				<div class="carousal-img-ctn"><img src="./img/nourish-black.jpg" class="carousal-img" alt="Dove Shampoo" /></div>
+				<div class="carousal-img-ctn last-img-ctn"><img src="./img/nourish-oil.jpg" class="carousal-img" alt="Dove Shampoo" /></div>
+			</div>
+		`;
+		return template;
 	}
 
 	function handleInput(e, answer){
 		e.preventDefault();
-		var chatOutputSection = $('#chat-output-section'), response = '';
+		var chatOutputSection = $('#chat-output-section');
 		switch(questionNo){
 			case 1:
 				if(answer === 'oily' || answer === 'dull'){
-					response = `
-						<div class="message flex-row flex-start">
-							<div class="bot-msg flex-row flex-center">
-								How many times do you wash your hair?
-							</div>
-						</div>
-					`;
+					var response1 = getBotMsgTemplate('How many times do you wash your hair?');
 					questionNo++;
-					sendResponse(chatOutputSection, response);
+					setTimeout(function(){
+						return sendResponse(chatOutputSection, response1);
+					}, 2000);
 				}
 				else if(answer === 'good'){
-					response = `
-						<div class="message flex-row flex-start">
-							<div class="bot-msg flex-row flex-center">
-								That is great!
-							</div>
-						</div>
-					`;
-					sendResponse(chatOutputSection, response);
-					response = `
-						<div class="message flex-row flex-start">
-							<div class="bot-msg flex-row flex-center">
-								I recommend you use dove oxygen moisture for even better results.
-							</div>
-						</div>
-					`;
+					var response1 = getBotMsgTemplate('That is great!');
 					setTimeout(function(){
-						return sendResponse(chatOutputSection, response);
+						return sendResponse(chatOutputSection, response1);
 					}, 2000);
-					var video = `
+					var response2 = getBotMsgTemplate('I recommend you use dove oxygen moisture for even better results.');
+					setTimeout(function(){
+						return sendResponse(chatOutputSection, response2);
+					}, 4000);
+					var response3 = `
 						<div class="message flex-row flex-start">
 							<iframe src="https://www.youtube.com/watch?v=wsoN2C0JzWk&autoplay=1" width="200px" height="200px"></iframe>
 						</div>
 					`;
 					setTimeout(function(){
-						return sendResponse(chatOutputSection, video);
-					}, 4000);
+						return sendResponse(chatOutputSection, response3);
+					}, 6000);
 				}
 				else{
-					response = `
-						<div class="message flex-row flex-start">
-							<div class="bot-msg flex-row flex-center">
-								I'm sorry I didn't get that
-							</div>
-						</div>
-					`;
-					sendResponse(chatOutputSection, response);
+					var response1 = getBotMsgTemplate('I\'m sorry I didn\'t get that');
+					setTimeout(function(){
+						return sendResponse(chatOutputSection, response1);
+					}, 2000);
+					var response2 = getBotMsgTemplate('Hi how are you');
+					setTimeout(function(){
+						return sendResponse(chatOutputSection, response2);
+					}, 4000);
 				}
 			break;
 			case 2:
 				if(answer <= 7){
-					var response1 = `
-						<div class="message flex-row flex-start">
-							<div class="bot-msg flex-row flex-center">
-								Washing your hair ` + answer + ` times per week when it is oily is not healthy
-							</div>
-						</div>
-					`;
-					sendResponse(chatOutputSection, response1);
-					var response2 = `
-						<div class="message flex-row flex-start">
-							<div class="bot-msg flex-row flex-center">
-								I recommend you use dove oil control shampoo
-							</div>
-						</div>
-					`;
+					var response1 = getBotMsgTemplate('Washing your hair ' + answer + ' times per week when it is oily is not healthy');
+					setTimeout(function(){
+						return sendResponse(chatOutputSection, response1);
+					}, 2000);
+					var response2 = getBotMsgTemplate('I recommend you use dove oil control shampoo');
 					setTimeout(function(){
 						return sendResponse(chatOutputSection, response2);
-					}, 2000);
-					var response3 = `
-						<div class="flex-row" id="carousal-ctn">
-							<div class="carousal-img-ctn"><img src="./img/dandruff.jpg" class="carousal-img" alt="Dove Shampoo" /></div>
-							<div class="carousal-img-ctn"><img src="./img/hairfall.jpg" class="carousal-img" alt="Dove Shampoo" /></div>
-							<div class="carousal-img-ctn"><img src="./img/nourish-black.jpg" class="carousal-img" alt="Dove Shampoo" /></div>
-							<div class="carousal-img-ctn last-img-ctn"><img src="./img/nourish-oil.jpg" class="carousal-img" alt="Dove Shampoo" /></div>
-						</div>
-					`;
+					}, 4000);
+					var response3 = getCarousalTemplate();
 					setTimeout(function(){
 						return sendResponse(chatOutputSection, response3);
-					}, 4000);
+					}, 6000);
 				}
 				else if(answer > 7){
-					response = `
-						<div class="message flex-row flex-start">
-							<div class="bot-msg flex-row flex-center">
-								Washing your hair ` + answer + ` times per week when it is dull is not healthy
-							</div>
-						</div>
-					`;
-					sendResponse(chatOutputSection, response);
+					var response1 = getBotMsgTemplate('Washing your hair ' + answer + ' times per week when it is dull is not healthy');
+					setTimeout(function(){
+						return sendResponse(chatOutputSection, response1);
+					}, 2000);
+					var response2 = getBotMsgTemplate('I recommend you use dove daily shine shampoo');
+					setTimeout(function(){
+						return sendResponse(chatOutputSection, response2);
+					}, 4000);
+					var response3 = getCarousalTemplate();
+					setTimeout(function(){
+						return sendResponse(chatOutputSection, response3);
+					}, 6000);
 				}
 				else{
-					response = `
-						<div class="message flex-row flex-start">
-							<div class="bot-msg flex-row flex-center">
-								I'm sorry I didn't get that
-							</div>
-						</div>
-					`;
-					sendResponse(chatOutputSection, response);
-					response = `
-						<div class="message flex-row flex-start">
-							<div class="bot-msg flex-row flex-center">
-								How many times do you wash your hair?
-							</div>
-						</div>
-					`;
+					var response1 = getBotMsgTemplate('I\'m sorry I didn\'t get that');
 					setTimeout(function(){
-						return sendResponse(chatOutputSection, response);
+						return sendResponse(chatOutputSection, response1);
 					}, 2000);
+					var response2 = getBotMsgTemplate('How many times do you wash your hair?');
+					setTimeout(function(){
+						return sendResponse(chatOutputSection, response2);
+					}, 4000);
 				}
 			break;
 		}
